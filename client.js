1c30ca64c04facf870e1ba187bad67d93703a40a
Vincent Bazia <xviniette@gmail.com>
2019-09-18 17:04:44 +0200

    ouais

--- client.js
+++ client.js
@@ -1,135 +1,176 @@
-const channelId = "459322521";
-const actionTime = 10000;
-let clicks = [];
-
-const forbiddenZones = [{ x: 0.96, y: 0, radius: 0.05 }]
-
-const circleRadius = 0.02;
-
-const canvas = document.querySelector("#heatmap");
-const ctx = canvas.getContext("2d");
-
-const gameFrame = {
-    x: 0.75,
-    y: 1
+const CHANNEL_ID = "459322521";
+const GAME_FRAME = {
+    x: 0,
+    y: 0,
+    width: 0.75,
+    height: 1
 }
+const AFK_TIME = 3 * 60 * 1000;
+const FORBIDDEN_ZONES = [{ x: 0.96, y: 0, radius: 0.05 }];
+const ACTION_TIME = 7000;
+
+let CLICKS = [];
+const CLICK_RADIUS = 0.02;
+const DBCLICK_RADIUS = 0.01;
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
 const distance = (x1, y1, x2, y2) => {
     return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))
 }
 
+//WEBSOCKET
 var socket = io("http://heat-ebs.j38.net/");
 
-socket.on('connect', () => {
-    console.log("connected");
-    socket.emit("channel", channelId);
+socket.on("connect", () => {
+    socket.emit("channel", CHANNEL_ID);
 });
 
-socket.on('click', data => {
+socket.on("click", data => {
     data = JSON.parse(data);
 
-    if (data.x > gameFrame.x) {
+    //GAME FRAME
+    if (data.x < GAME_FRAME.x) {
+        return;
+    }
+    if (data.x > GAME_FRAME.x + GAME_FRAME.width) {
+        return;
+    }
+    if (data.y < GAME_FRAME.y) {
+        return;
+    }
+    if (data.y > GAME_FRAME.y + GAME_FRAME.height) {
         return;
     }
 
-    data.double = false;
     data.x *= 1;
     data.y *= 1;
+    data.dbclick = false;
 
-    data.x /= gameFrame.x;
+    data.x /= GAME_FRAME.width;
+    data.y /= GAME_FRAME.height;
 
-    if (forbiddenZones.find(f => distance(data.x, data.y, f.x, f.y) < f.radius)) {
+    //FORBIDDEN
+    if (FORBIDDEN_ZONES.find(f => distance(data.x, data.y, f.x, f.y) < f.radius)) {
         return;
     }
 
-    const click = clicks.find(c => c.id == data.id);
+    //USER CLICK
+    const click = CLICKS.find(c => c.id == data.id);
     if (click) {
-        if (distance(click.x, click.y, data.x, data.y) < 0.01) {
-            click.double = true;
+        if (distance(click.x, click.y, data.x, data.y) < DBCLICK_RADIUS) {
+            click.dbclick = true;
         } else {
-            click.double = false;
             click.x = data.x;
             click.y = data.y;
+            click.dbclick = false;
         }
     } else {
-        clicks.push(data);
+        CLICKS.push(data);
     }
 });
 
-socket.on('disconnect', () => {
-    console.log("disconnected")
-});
-
-const getDemocraticPoint = (radius = 0.01) => {
-    if (clicks.length == 0) {
+const getCommunityClick = () => {
+    if (CLICKS.length == 0) {
         return null;
     }
 
-    clicks.forEach(c => c.weight = 1);
+    CLICKS.forEach(c => c.weight = 1);
 
-    for (let i = 0; i < clicks.length; i++) {
-        for (let j = i + 1; j < clicks.length; j++) {
-            if (clicks[i].double == clicks[i].double && distance(clicks[i].x, clicks[i].y, clicks[j].x, clicks[j].y) < radius * 2) {
-                clicks[i].weight++;
-                clicks[j].weight++;
+    for (let i = 0; i < CLICKS.length; i++) {
+        for (let j = i + 1; j < CLICKS.length; j++) {
+            if (CLICKS[i].dbclick == CLICKS[i].dbclick && distance(CLICKS[i].x, CLICKS[i].y, CLICKS[j].x, CLICKS[j].y) < CLICK_RADIUS * 2) {
+                CLICKS[i].weight++;
+                CLICKS[j].weight++;
             }
         }
     }
 
-    clicks = clicks.sort((a, b) => b.weight - a.weight);
-    return clicks[0];
+    CLICKS = CLICKS.sort((a, b) => b.weight - a.weight);
+    return CLICKS[0];
+}
+
+const sendActions = (actions = []) => {
+    fetch("http://localhost:3000/", {
+        method: "POST",
+        body: JSON.stringify({ actions }),
+        headers: { 'Content-type': 'application/json' }
+    });
 }
 
 let lastAction = Date.now();
-const step = () => {
+let lastClick = Date.now();
+const update = () => {
     const now = Date.now();
-    if (now > lastAction + actionTime) {
+    if (now > lastAction + ACTION_TIME) {
         lastAction = now;
 
-        const point = getDemocraticPoint(circleRadius);
-        if (point) {
-            fetch(`http://localhost:3000/${(point.x) * gameFrame.x + 0.125}/${point.y}/${point.double ? 1 : 0}`);
+        const click = getCommunityClick();
+        if (click) {
+            sendActions([
+                {
+                    action: "moveMouseSmooth",
+                    x: click.x * GAME_FRAME.width + 0.125,
+                    y: click.y
+                },
+                {
+                    action: "mouseClick",
+                    double: click.dbclick
+                },
+                {
+                    action: "moveMouse",
+                    x: 0.46,
+                    y: 0.92
+                }
+            ]);
+
+
+            lastClick = Date.now();
+        } else {
+            if (now > lastClick + AFK_TIME) {
+
+                sendActions([
+                    {
+                        action: "moveMouseSmooth",
+                        x: Math.random() * 0.2 + 0.4,
+                        y: Math.random() * 0.2 + 0.4
+                    },
+                    {
+                        action: "mouseClick",
+                    }
+                ]);
+
+                lastClick = Date.now();
+            }
         }
 
-        clicks = [];
+        CLICKS = [];
     }
 
-    //Draw
+    //CLEAR
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    //Background
-    ctx.fillStyle = "rgb(0, 0, 255)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    //forbidden
-    //  forbiddenZones.forEach(click => {
-    //     ctx.fillStyle = "rgba(255, 255, 0, 1)";
-    //     ctx.beginPath();
-    //     ctx.arc(click.x * canvas.width, click.y * canvas.height, canvas.width * click.radius, 0, 2 * Math.PI);
-    //     ctx.fill();
-    // });
-
-    //Clicks
-    clicks.forEach(click => {
-        if(click.double){
-            ctx.fillStyle = "rgba(255, 0, 0, 1)";
-        }else{
-            ctx.fillStyle = "rgba(255, 255, 0, 1)";
+    //CLICKS
+    ctx.globalCompositeOperation = "multiply";
+    CLICKS.forEach(click => {
+        if (click.dbclick) {
+            ctx.fillStyle = "rgb(255, 191, 184)";
+        } else {
+            ctx.fillStyle = "rgb(255, 242, 184)";
         }
         ctx.beginPath();
-        ctx.arc(click.x * canvas.width, click.y * canvas.height, canvas.width * circleRadius, 0, 2 * Math.PI);
+        ctx.arc(click.x * canvas.width, click.y * canvas.height, canvas.width * CLICK_RADIUS, 0, 2 * Math.PI);
         ctx.fill();
     });
 
-    //Timer
-    ratio = (now - lastAction) / actionTime;
+    //TIMER
+    ratio = (now - lastAction) / ACTION_TIME;
     ctx.fillStyle = `rgb(255, ${255 * (1 - ratio)}, 0)`;
     ctx.fillRect(canvas.width - 10, 0, 10, canvas.height * ratio);
 
-    requestAnimationFrame(step);
+    requestAnimationFrame(update);
 }
-
-requestAnimationFrame(step);
 
 
 const resize = () => {
@@ -138,8 +179,8 @@ const resize = () => {
 }
 
 window.addEventListener('resize', resize, false);
-resize();
 
 window.onload = () => {
     resize();
+    update();
 }
--- robot.js
+++ robot.js
@@ -1,22 +1,70 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const robot = require("robotjs");
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Range, Accept-Range");
+    res.header("Access-Control-Expose-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Range, Accept-Range");
+    next();
+});
 
 let toggle = true;
 
-app.get('/:x/:y/:dbclick?', function (req, res) {
-    if (toggle) {
-        robot.moveMouseSmooth(req.params.x * robot.getScreenSize().width, req.params.y * robot.getScreenSize().height);
-        robot.mouseClick("left", req.params.dbclick == "1" ? true : false);
-    }
-    res.send();
-});
-
-app.get('/toggle', function (req, res) {
+app.get("/toggle", function (req, res) {
     toggle = !toggle;
     res.send(toggle);
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.post("/", (req, res) => {
+    if (!toggle) {
+        return;
+    }
+
+    if (!req.body.actions) {
+        return;
+    }
+
+
+    req.body.actions.forEach(action => {
+        let params = {
+            x: 0,
+            y: 0,
+            button: "left",
+            double: false,
+            down: "down",
+        }
+
+        Object.assign(params, action);
+
+        switch (action.action) {
+            case "moveMouse":
+                robot.moveMouse(params.x * robot.getScreenSize().width, params.y * robot.getScreenSize().height);
+                break;
+            case "moveMouseSmooth":
+                robot.moveMouseSmooth(params.x * robot.getScreenSize().width, params.y * robot.getScreenSize().height);
+                break;
+            case "mouseClick":
+                robot.mouseClick(params.button, params.double);
+                break;
+            case "mouseToggle":
+                robot.mouseToggle(params.down, params.button);
+                break;
+            case "dragMouse":
+                robot.dragMouse(params.x * robot.getScreenSize().width, params.y * robot.getScreenSize().height);
+                break;
+        }
+    });
+
+
+    res.status(200).json(req.body);
+});
+
+app.listen(3000, () => {
+    console.log('ROBOTJS')
 })
